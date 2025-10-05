c454af4df061be744e3af319a6dc72789d3b2559
WNomunomu <kohsei.nmr@gmail.com>
2025-10-05 16:11:56 +0900

    Add nvim configurations to introduce peek.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -388,5 +388,20 @@ return {
       })
     end,
   },
+  {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+
+    config = function()
+        require("peek").setup({
+          app = "browser",
+        })
+        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+        vim.keymap.set('n', '<leader>mo', require("peek").open, { desc = "Peek Open" })
+        vim.keymap.set('n', '<leader>mc', require("peek").close, { desc = "Peek Close" })
+    end,
+  },
 }
 
