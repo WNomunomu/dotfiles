09deb40feacebc9bda670b6598e7d221071f191b
WNomunomu <kohsei.nmr@gmail.com>
2025-10-05 17:05:39 +0900

    Comment out configurations of toggleterm

--- .config/nvim/lua/plugins/tools/toggleterm.lua
+++ .config/nvim/lua/plugins/tools/toggleterm.lua
@@ -1,29 +1,30 @@
 return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  keys = {
-    { "<leader>to", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
-  },
-  config = function()
-    require("toggleterm").setup({
-      size = 100,
-      hide_numbers = true,
-      shade_filetypes = {},
-      shade_terminals = true,
-      shading_factor = 2,
-      start_in_insert = true,
-      insert_mappings = true,
-      persist_size = true,
-      direction = "float",
-      close_on_exit = true,
-    })
+--   "akinsho/toggleterm.nvim",
+--   version = "*",
+--   keys = {
+--     { "<leader>to", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
+--   },
+--   config = function()
+--     require("toggleterm").setup({
+--       size = 100,
+--       hide_numbers = true,
+--       shade_filetypes = {},
+--       shade_terminals = true,
+--       shading_factor = 2,
+--       start_in_insert = true,
+--       insert_mappings = true,
+--       persist_size = true,
+--       direction = "float",
+--       close_on_exit = true,
+--     })
 
-    -- ターミナルモードでのキーマッピング
-    function _G.set_terminal_keymaps()
-      local opts = { buffer = 0 }
-      vim.keymap.set("t", "<C-c>", [[<C-\><C-n>:ToggleTerm<CR>]], opts)
-    end
+--     -- ターミナルモードでのキーマッピング
+--     function _G.set_terminal_keymaps()
+--       local opts = { buffer = 0 }
+--       vim.keymap.set("t", "<C-c>", [[<C-\><C-n>:ToggleTerm<CR>]], opts)
+--     end
 
-    vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
-  end,
+--     vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+--   end,
 }
+
