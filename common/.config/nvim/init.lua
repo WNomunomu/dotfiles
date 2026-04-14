7182c26fc8c3940caf4e1af54fd5a8cb269016b1
WNomunomu <kohsei.nmr@gmail.com>
2026-04-14 22:41:26 +0900

    Add Neovim terminal colors and window navigation keymaps

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -26,6 +26,25 @@ vim.api.nvim_create_autocmd('VimLeave', {
   end,
 })
 
+-- Alacritty の github_dark_tritanopia テーマに合わせたターミナルカラー
+-- nvim 内蔵ターミナル (lazygit 等) で Alacritty と同じ色を使うため
+vim.g.terminal_color_0  = '#484f58'  -- black
+vim.g.terminal_color_1  = '#ff7b72'  -- red
+vim.g.terminal_color_2  = '#58a6ff'  -- green
+vim.g.terminal_color_3  = '#d29922'  -- yellow
+vim.g.terminal_color_4  = '#58a6ff'  -- blue
+vim.g.terminal_color_5  = '#bc8cff'  -- magenta
+vim.g.terminal_color_6  = '#39c5cf'  -- cyan
+vim.g.terminal_color_7  = '#b1bac4'  -- white
+vim.g.terminal_color_8  = '#6e7681'  -- bright black
+vim.g.terminal_color_9  = '#ffa198'  -- bright red
+vim.g.terminal_color_10 = '#79c0ff'  -- bright green
+vim.g.terminal_color_11 = '#e3b341'  -- bright yellow
+vim.g.terminal_color_12 = '#79c0ff'  -- bright blue
+vim.g.terminal_color_13 = '#bc8cff'  -- bright magenta
+vim.g.terminal_color_14 = '#39c5cf'  -- bright cyan
+vim.g.terminal_color_15 = '#b1bac4'  -- bright white
+
 vim.cmd([[
   highlight Normal guibg=NONE ctermbg=NONE
   highlight NormalNC guibg=NONE ctermbg=NONE
--- common/.config/nvim/lua/config/keymap.lua
+++ common/.config/nvim/lua/config/keymap.lua
@@ -6,6 +6,12 @@ keymap('i', 'jj', '<Esc>', { silent = true })
 keymap('n', '<C-a>', 'ggVG', { silent = true })
 keymap('n', '<leader>m', '<cmd>Mason<cr>', {})
 
+-- ターミナルモード内でも Ctrl-w + hjkl でウィンドウ移動できるようにする
+vim.keymap.set("t", "<C-w>h", "<C-\\><C-n><C-w>h", { desc = "Move to left window" })
+vim.keymap.set("t", "<C-w>j", "<C-\\><C-n><C-w>j", { desc = "Move to lower window" })
+vim.keymap.set("t", "<C-w>k", "<C-\\><C-n><C-w>k", { desc = "Move to upper window" })
+vim.keymap.set("t", "<C-w>l", "<C-\\><C-n><C-w>l", { desc = "Move to right window" })
+
 -- LSPキーマップ
 
 vim.api.nvim_create_autocmd('LspAttach', {
