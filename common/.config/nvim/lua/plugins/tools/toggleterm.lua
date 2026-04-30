59a33b138f48807d22975b96bbfc32cf7d93e464
WNomunomu <kohsei.nmr@gmail.com>
2026-04-30 12:43:00 +0900

    Add toggleterm plugin

--- common/.config/nvim/lua/plugins/tools/toggleterm.lua
+++ common/.config/nvim/lua/plugins/tools/toggleterm.lua
@@ -1,30 +1,32 @@
 return {
---   "akinsho/toggleterm.nvim",
---   version = "*",
---   keys = {
---     { "<leader>to", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
---   },
---   config = function()
---     require("toggleterm").setup({
---       size = 100,
---       hide_numbers = true,
---       shade_filetypes = {},
---       shade_terminals = true,
---       shading_factor = 2,
---       start_in_insert = true,
---       insert_mappings = true,
---       persist_size = true,
---       direction = "float",
---       close_on_exit = true,
---     })
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  keys = {
+    { "<leader>to", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
+  },
+  config = function()
+    require("toggleterm").setup({
+      size = 15,
+      hide_numbers = true,
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      persist_size = true,
+      direction = "horizontal",
+      close_on_exit = true,
+    })
 
---     -- ターミナルモードでのキーマッピング
---     function _G.set_terminal_keymaps()
---       local opts = { buffer = 0 }
---       vim.keymap.set("t", "<C-c>", [[<C-\><C-n>:ToggleTerm<CR>]], opts)
---     end
+    -- ターミナルモードでのキーマッピング
+    function _G.set_terminal_keymaps()
+      local opts = { buffer = 0 }
+      vim.keymap.set("t", "<C-t>", [[<C-\><C-n>:ToggleTerm<CR>]], opts)
+      vim.keymap.set("t", "<C-w>h", [[<C-\><C-n><C-w>h]], opts)
+      vim.keymap.set("t", "<C-w>j", [[<C-\><C-n><C-w>j]], opts)
+      vim.keymap.set("t", "<C-w>k", [[<C-\><C-n><C-w>k]], opts)
+      vim.keymap.set("t", "<C-w>l", [[<C-\><C-n><C-w>l]], opts)
+    end
 
---     vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
---   end,
+    vim.cmd("autocmd! TermOpen term://*toggleterm* lua set_terminal_keymaps()")
+  end,
 }
-
